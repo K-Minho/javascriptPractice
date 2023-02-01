8738cac736e137c09b787dd6581681068ebd3ae1
K-Minho <alsgh9979@naver.com>
2023-02-01 14:10:25 +0900

    02-01 오전

---
+++ ex02/funex01.js
@@ -0,0 +1,19 @@
+
+let num1 = 3;
+
+let f = function (num2) {
+    console.log("run1 :", num1);
+    console.log("run2 :", num2);
+    return 10;
+}
+
+let sum = f(5);
+
+console.log(sum);
+
+let a = function (x, y) {
+    return z;
+}
+
+let b = () => { };
+let c = () => 1; //return 1;
---
+++ ex02/varex01.js
@@ -0,0 +1,18 @@
+let n1 = 1;
+let d1 = 1.5;
+let b1 = true;
+let c1 = "a";
+let arr = [1, 2, "가"];
+let user = {
+    id: 1,
+    username: "ssar",
+    email: "ssar@email.com",
+    hobby: ["농구", "축구"],
+    account: {
+        id: 1122,
+        password: 8087,
+        balance: 50000
+    }
+};
+
+console.log(user);
